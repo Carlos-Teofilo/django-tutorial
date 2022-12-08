fba60584771e1481ffc296e4b30ea147f183c3d0
Carlos Teofilo <carlosneto2810@hotmail.com>
2022-12-08 15:07:00 -0400

    FORMAT

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-# from django.template import loader
 
 from .models import Question
+
+# from django.template import loader
 
 
 def index(request):
